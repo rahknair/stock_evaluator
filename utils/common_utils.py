cbaa8a007c49131cb84458a2a235a345e711e9fc
mr.black <rahulknairvnmy@gmail.com>
2024-03-28 23:04:46 +0530

    Removed un-wanted codes from fetching the current stock value ; the value error is comming some times due to the server error of yfinance library

--- utils/common_utils.py
+++ utils/common_utils.py
@@ -1,23 +1,22 @@
-def extract_current_price(input=None):
-    """
-    This function will check and give the current price of the stock
-    :return: the current price of the stock
-    """
-    import yfinance as yf
+import yfinance as yf
 
-    # Specify the stock symbol (ticker)
-    #stock_symbol = "ICICIBANK.NS"
-    stock_symbol = input
+class CommonUtils:
 
-    # Get stock information
-    ticker = yf.Ticker(stock_symbol)
-    print(ticker.info)
-    current_price = ticker.info["currentPrice"]
+    def __init__(self):
+        """
+        Initializing parametrs
+        """
 
-    # Print the result
-    print(f"Stock: {stock_symbol}")
-    print(f"Current Price: Rs.{current_price:.2f}")
-    return current_price
+    def extract_current_price(self, input=None):
+        """
+        This function will check and give the current price of the stock
+        :return: the current price of the stock
+        """
+        # Specify the stock symbol (ticker)
+        #stock_symbol = "ICICIBANK.NS"
+        stock_symbol = input
 
-
-
+        # Get stock information
+        ticker = yf.Ticker(stock_symbol)
+        current_price = ticker.info["currentPrice"]
+        return current_price
